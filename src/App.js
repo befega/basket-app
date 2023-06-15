64127f855837a6bb748bdb7bd90a1683ba2d47ac
befega <befega@live.com>
2023-06-16 01:54:57 +0300

    tema ve card comp eklendi. butonlar aktif

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Container, SimpleGrid, List, ThemeIcon, Stack } from "@mantine/core";
+import { IconCircleCheck } from "@tabler/icons-react";
+import Card from "./components/card";
+
+const storeItems = [
+  {
+    name: "Klavye",
+    price: 100,
+    quantity: 5,
+    image:
+      "https://unsplash.com/photos/2xU7rYxsTiM/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8a2V5Ym9hcmR8ZW58MHx8fHwxNjg2ODE4MDExfDA&force=true&w=640",
+    description:
+      "Bu klavye 102 tuşa sahiptir. Arkadan aydınlatmalı RGB renklere sahiptir ve her bir tuş kontrol edilebilir.",
+  },
+  {
+    name: "Mouse",
+    price: 200,
+    quantity: 7,
+    image:
+      "https://unsplash.com/photos/T_2UFB_xwzw/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Nnx8Y29tcHV0ZXIlMjBtb3VzZXxlbnwwfHx8fDE2ODY4MzkyMzR8MA&force=true&w=640",
+    description:
+      "Bu mouse 6000 DPI özellikli ve ele tam oturan kaliteli bir oyuncu ekipmanıdır.",
+  },
+  {
+    name: "Hoparlör",
+    price: 300,
+    quantity: 10,
+    image:
+      "https://unsplash.com/photos/qPojqUji_y4/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Nnx8Y29tcHV0ZXIlMjBzcGVha2VyfGVufDB8fHx8MTY4Njg1NzYxMHww&force=true&w=640",
+    description:
+      "Bu hoparlör 2+1 15W gücünde ve 3.5mm ses çıkışına sahip iyi bir hoparlördür.",
+  },
+];
 
 function App() {
+  let [basketItems, setBasketItems] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Stack
+        align="flex-start"
+        justify="flex-start"
+        h={300}
+        sx={(theme) => ({
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        })}
+      >
+        <SimpleGrid cols={3}>
+          {storeItems.map(({ name, image, price, quantity, description }) => {
+            return (
+              <Card
+                key={name}
+                name={name}
+                image={image}
+                price={price}
+                quantity={quantity}
+                description={description}
+                onAdd={() => setBasketItems([...basketItems, { name }])}
+              />
+            );
+          })}
+        </SimpleGrid>
+        <SimpleGrid>
+          <List
+            spacing="xs"
+            size="sm"
+            center
+            icon={
+              <ThemeIcon color="teal" size={24} radius="xl">
+                <IconCircleCheck size="1rem" />
+              </ThemeIcon>
+            }
+          >
+            {basketItems.map(({ name }, index) => (
+              <List.Item key={index}>{name}</List.Item>
+            ))}
+          </List>
+        </SimpleGrid>
+      </Stack>
+    </Container>
   );
 }
 
---
+++ src/components/card.js
@@ -0,0 +1,45 @@
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+
+const CardComponent = ({
+  name,
+  image,
+  price,
+  quantity,
+  description,
+  onAdd,
+}) => {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image src={image} height={160} alt={name} />
+      </Card.Section>
+
+      <Group position="apart" mt="md" mb="xs">
+        <Text weight={500}>{name}</Text>
+        <Badge color="pink" variant="light">
+          {price} TL
+        </Badge>
+        <Badge color="blue" variant="light">
+          Stok: {quantity}
+        </Badge>
+      </Group>
+
+      <Text size="sm" color="black">
+        {description}
+      </Text>
+
+      <Button
+        variant="light"
+        color="blue"
+        fullWidth
+        mt="md"
+        radius="md"
+        onClick={onAdd}
+      >
+        Ekle
+      </Button>
+    </Card>
+  );
+};
+
+export default CardComponent;
